fe8d576f3323f487947abf1e8e04af299102f389
= <gtaspanow2513@gmail.com>
2024-10-23 06:55:13 -0500

    informe Erick

--- Ejercicio4/src/condicional1_5.java
+++ Ejercicio4/src/condicional1_5.java
@@ -1,23 +1,14 @@
-
-/*
-* Ejemplo básico java
-* Programa con estructura condicional
-* Programa que lea dos caracteres y compruebe
-* si son iguales.
-*/
 import java.io.*;
-
 public class condicional1_5 {
     public static void main(String[] args) throws IOException {
         char car1, car2;
         System.out.print("Introduzca primer carácter: ");
-        car1 = (char) System.in.read(); // lee un carácter
-        System.in.read(); // saltar el intro que ha quedado en el buffer
+        car1 = (char) System.in.read();
+        System.in.read();
         System.out.print("Introduzca segundo carácter: ");
-        car2 = (char) System.in.read(); // lee el segundo carácter
+        car2 = (char) System.in.read();
         if (car1 == car2)
             System.out.println("Son iguales");
-
         else
             System.out.println("No son iguales");
     }
--- Ejercicio5/src/condicional1_5.java
+++ Ejercicio5/src/condicional1_5.java
@@ -1,18 +1,12 @@
 import java.io.*;
-
-public class condicional1_5 {
-    public static void main(String[] args) throws IOException {
-        char car1, car2;
-        System.out.print("Introduzca primer carácter: ");
-        car1 = (char) System.in.read(); // lee un carácter
-        System.in.read(); // saltar el intro que ha quedado en el buffer
-        System.out.print("Introduzca segundo carácter: ");
-        car2 = (char) System.in.read(); // lee el segundo carácter
-        Character c1 = new Character(car1);
-        Character c2 = new Character(car2);
-        if (c1.equals(c2)) // comparamos dos objetos Character mediante el método equals
-            System.out.println("Son iguales");
-        else
-            System.out.println("No son iguales");
-    }
+public class condicional1_7 {
+public static void main(String[] args) throws IOException {
+char car1;
+System.out.print("Introduzca carácter: ");
+car1 = (char)System.in.read();
+if(Character.isDigit(car1))
+System.out.println("Es un número");
+else
+System.out.println("No es un número");
+}
 }
